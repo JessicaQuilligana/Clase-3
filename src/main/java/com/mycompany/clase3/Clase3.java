f9ba18837298a3f478830bc60fd447404a7c8fa3
Jessica Quilligana <quilliganaj351@gmail.com>
2025-04-02 11:00:36 -0500

    ejemplo

---
+++ src/main/java/com/mycompany/clase3/Clase3.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clase3;
+
+/**
+ *
+ * @author quill
+ */
+public class Clase3 {
+
+    public static void main(String[] args) {
+    //ejercicio 1
+        /*double base=2;
+        double altura=4;
+        double area=0;
+        area=base*altura;
+        System.out.println("La altura del rectangulo es:"+area);*/
+        //Ejercicio 2
+        /*int num=2;
+        if (num>0) {
+                System.out.println("El número es positivo:"+num);
+            } else {
+            System.out.println("El número es negativo");
+            }*/
+        //Ejercico 3
+        /*int num=5;
+        int operacion=num%2;
+        if (operacion==0) {
+            System.out.println("El número es par:"+num);
+        } else {
+            System.out.println("El número es impar");
+        }*/
+        //Ejercicio 4
+        int nota=80;
+        if ((nota >=0)&&(nota<=100));
+        {
+         if (nota>=90){
+            System.out.println("Execelente");
+        }
+        else if((nota>=70)&& (nota<=89));
+        {
+            System.out.println("Aprobado");
+            } 
+        else if(nota<70)  
+        {
+            System.out.println("Reprobado");
+        }
+            else {
+                System.out.println("Su nota es invalida");
+                }
+        } 
+    }    
+//Sintaxis de do while
+        /*int contador=5;
+        do {            
+            System.out.println("Hola: "+ contador);
+            contador++;
+        } while (contador<5);
+        //Sintaxys de while
+        /*while (contador<5) {
+            System.out.println("Hola "+contador);
+            //contador=contador+3;
+            contador++;*/
+            
+        }
+    }
+
